0acbfce40febfc8891d2fb8137592ffcb99b7b3c
Abi-Vike <tomy0abr@gmail.com>
2023-06-21 17:55:06 +0300

    cleared up some mess from images folders and kinda successful ID generation

--- user/payment-verify.php
+++ user/payment-verify.php
@@ -12,6 +12,84 @@ use phpMailer\PHPMailer\PHPMailer;
 use phpMailer\PHPMailer\SMTP;
 //use phpMailer\PHPMailer\Exception;
 
+// ID card generator starts here
+
+//$id_no = $_POST['id_no'];
+$sql = "Select * from cards where id_no='1005' ";
+$result = mysqli_query($con, $sql);
+if (mysqli_num_rows($result) > 0) {
+  $html = "<div class='card' style='width:350px; padding:0;'>";
+  $html .= "";
+  while ($row = mysqli_fetch_assoc($result)) {
+
+    $name = $row["name"];
+    $id_no = $row["id_no"];
+    $grade = $row['grade'];
+    $dob = $row['dob'];
+    $address = $row['address'];
+    $email = $row['email'];
+    $exp_date = $row['exp_date'];
+    $phone = $row['phone'];
+    $address = $row['address'];
+    $image = $row['image'];
+    $date = date('M d, Y', strtotime($row['date']));
+
+    $html .= "
+      <div class='container-0' style='text-align:left; border:2px dashed black;'>
+        <div class='header'>
+        </div>
+        <div class='container-2'>
+          <div class='box-1'>
+            <img src='$image' />
+          </div>
+          <div class='box-2'>
+            <h2>$name</h2>
+            <p style='font-size: 14px;'>Student</p>
+          </div>
+          <div class='box-3'>
+            <img src='../images/rvu-logo.png' alt=''>
+          </div>
+        </div>
+
+        <div class='container-3'>
+          <div class='info-1'>
+            <div class='id'>
+              <h4>ID Number</h4>
+              <p>$id_no</p>
+            </div>
+            <div class='dob'>
+              <h4>Phone</h4>
+              <p>$phone</p>
+            </div>
+          </div>
+          <div class='info-2'>
+            <div class='join-date'>
+              <h4>ID Issue Date</h4>
+              <p>$date</p>
+            </div>
+            <div class='expire-date'>
+              <h4>ID Expire Date</h4>
+              <p>$exp_date</p>
+            </div>
+          </div>
+          <div class='info-3'>
+            <div class='email'>
+              <h4>Address</h4>
+              <p>$address this is the final long address</p>
+            </div>
+          </div>
+          <div class='info-4'>
+            <div class='sign'>
+              <br>
+              <p style='font-size:12px;'>Your Signature</p>
+            </div>
+          </div>
+        </div>
+      </div>";
+  }
+}
+// ID card generator ends here
+
 if (isset($_GET['uid'])) {
   $uid = $_GET['uid'];
   // need to extract out the application ID from tbladmapplications as I only have the userID here
@@ -76,6 +154,237 @@ if (isset($_GET['uid'])) {
     <link rel="stylesheet" type="text/css" href="app-assets/css/vendors.css">
     <link rel="stylesheet" type="text/css" href="app-assets/css/app.css">
     <link rel="stylesheet" type="text/css" href="app-assets/css/core/menu/menu-types/vertical-menu-modern.css">
+
+    <style>
+      .lavkush img {
+        border-radius: 8px;
+        border: 2px solid blue;
+      }
+
+      span {
+        font-family: 'Orbitron', sans-serif;
+        font-size: 16px;
+      }
+
+      hr.new2 {
+        border-top: 1px dashed black;
+        width: 350px;
+        text-align: left;
+        align-items: left;
+      }
+
+      p {
+        font-size: 13px;
+        margin-top: -5px;
+      }
+
+      .container-0 {
+        width: 80vh;
+        height: 45vh;
+        margin: auto;
+        background-color: white;
+        box-shadow: 0 1px 10px rgb(146 161 176 / 50%);
+        overflow: hidden;
+        border-radius: 10px;
+      }
+
+      .header {
+        /* border: 2px solid black; */
+        width: 73vh;
+        height: 15vh;
+        margin: 20px auto;
+        background-color: white;
+        /* box-shadow: 0 1px 10px rgb(146 161 176 / 50%); */
+        /* border-radius: 10px; */
+        background-image: url(../images/rvu-poster.png);
+        overflow: hidden;
+        font-family: 'Poppins', sans-serif;
+      }
+
+      .header h1 {
+        color: rgb(27, 27, 49);
+        text-align: right;
+        margin-right: 20px;
+        margin-top: 15px;
+      }
+
+      .header p {
+        color: rgb(157, 51, 0);
+        text-align: right;
+        margin-right: 22px;
+        margin-top: -10px;
+      }
+
+      .container-2 {
+        /* border: 2px solid red; */
+        width: 73vh;
+        height: 10vh;
+        margin: 0px auto;
+        margin-top: -20px;
+        display: flex;
+      }
+
+      .box-1 {
+        border: 4px solid black;
+        width: 90px;
+        height: 95px;
+        margin: -40px 25px;
+        border-radius: 3px;
+      }
+
+      .box-1 img {
+        width: 82px;
+        height: 87px;
+      }
+
+      .box-2 {
+        /* border: 2px solid purple; */
+        width: 33vh;
+        height: 8vh;
+        margin: 7px 0px;
+        padding: 5px 7px 0px 0px;
+        text-align: left;
+        font-family: 'Poppins', sans-serif;
+      }
+
+      .box-2 h2 {
+        font-size: 1.3rem;
+        margin-top: -5px;
+        color: rgb(27, 27, 49);
+        ;
+      }
+
+      .box-2 p {
+        font-size: 0.7rem;
+        margin-top: -5px;
+        color: rgb(179, 116, 0);
+      }
+
+      .box-3 {
+        /* border: 2px solid rgb(21, 255, 0); */
+        width: 8vh;
+        height: 8vh;
+        margin: 8px 0px 8px 30px;
+      }
+
+      .box-3 img {
+        width: 8vh;
+      }
+
+      .container-3 {
+        /* border: 2px solid rgb(111, 2, 161); */
+        width: 73vh;
+        height: 12vh;
+        margin: 0px auto;
+        margin-top: 10px;
+        display: flex;
+        font-family: 'Shippori Antique B1', sans-serif;
+        font-size: 0.7rem;
+      }
+
+      .info-1 {
+        /* border: 1px solid rgb(255, 38, 0); */
+        width: 17vh;
+        height: 12vh;
+      }
+
+      .id {
+        /* border: 1px solid rgb(2, 92, 17); */
+        width: 17vh;
+        height: 5vh;
+      }
+
+      .id h4 {
+        color: rgb(179, 116, 0);
+        font-size: 15px;
+      }
+
+      .dob {
+        /* border: 1px solid rgb(0, 46, 105); */
+        width: 17vh;
+        height: 5vh;
+        margin: 8px 0px 0px 0px;
+      }
+
+      .dob h4 {
+        color: rgb(179, 116, 0);
+        font-size: 15px;
+      }
+
+      .info-2 {
+        /* border: 1px solid rgb(4, 0, 59); */
+        width: 17vh;
+        height: 12vh;
+      }
+
+      .join-date {
+        /* border: 1px solid rgb(2, 92, 17); */
+        width: 17vh;
+        height: 5vh;
+      }
+
+      .join-date h4 {
+        color: rgb(179, 116, 0);
+        font-size: 15px;
+      }
+
+      .expire-date {
+        /* border: 1px solid rgb(0, 46, 105); */
+        width: 17vh;
+        height: 5vh;
+        margin: 8px 0px 0px 0px;
+      }
+
+      .expire-date h4 {
+        color: rgb(179, 116, 0);
+        font-size: 15px;
+      }
+
+      .info-3 {
+        /* border: 1px solid rgb(255, 38, 0); */
+        width: 17vh;
+        height: 12vh;
+      }
+
+      .email {
+        /* border: 1px solid rgb(2, 92, 17); */
+        width: 22vh;
+        height: 5vh;
+      }
+
+      .email h4 {
+        color: rgb(179, 116, 0);
+        font-size: 15px;
+      }
+
+      .phone {
+        /* border: 1px solid rgb(0, 46, 105); */
+        width: 17vh;
+        height: 5vh;
+        margin: 8px 0px 0px 0px;
+      }
+
+      .info-4 {
+        /* border: 2px solid rgb(255, 38, 0); */
+        width: 22vh;
+        height: 12vh;
+        margin: 0px 0px 0px 0px;
+        font-size: 15px;
+      }
+
+      .phone h4 {
+        color: rgb(179, 116, 0);
+        font-size: 15px;
+      }
+
+      .sign {
+        /* border: 1px solid rgb(0, 46, 105); */
+        width: 17vh;
+        height: 5vh;
+        margin: 41px 0px 0px 20px;
+        text-align: center;
+      }
+    </style>
   </head>
 
   <body class="vertical-layout vertical-menu-modern 2-columns menu-expanded fixed-navbar" data-open="click" data-menu="vertical-menu-modern" data-col="2-columns">
@@ -90,8 +399,10 @@ if (isset($_GET['uid'])) {
     <div class="app-content content">
       <div class="content-wrapper">
         <div class="content-header row"></div>
+
         <div class="content-body">
-          <?php if ($pay_data['Pay_Confirmed'] === 'verified') {
+          <?php
+          if ($pay_data['Pay_Confirmed'] === 'verified') {
             // fetch data from tblregistered
             $fetch_reg = mysqli_query($con, "SELECT * FROM tblregistered WHERE Reg_User_ID = '$uid'");
             $row = mysqli_fetch_array($fetch_reg);
@@ -121,11 +432,16 @@ if (isset($_GET['uid'])) {
                     </a>
                   </div>
                 </div>
+                <hr>
+                <h3>Your ID:</h3>
+                <div class="card-body" id="mycard">
+                  <?php echo $html ?>
+                </div>
               </div>
             </div>
-            <hr>
-            
-          <?php } else { ?>
+
+          <?php
+          } else { ?>
             <h4>
               <strong>Payment reference:</strong> <?php echo $pay_data['Pay_Ref'] ?> <br><br>
               <strong>Payment Date:</strong> <?php echo date('d-M-Y', strtotime($pay_data['Pay_Date'])) ?> <br><br>
